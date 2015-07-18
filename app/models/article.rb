a7c016251a8ec6f50fcdece7fecba762cb922556
buirai <buirai_naetle@hotmail.com>
2015-07-18 16:20:23 -0500

    show error message to user

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,12 @@ class Article < ActiveRecord::Base
 	# la tabla ==> articles
 	#campos ==> article.title() ==> 'El título'
 	#Escribir métodos
-	validates :title, presence: true #validates the title if is null
-	validates :body, presence: true #validates the body if is null
+
+	#validates the title if is null
+	#validates the title if is unique
+	validates :title, presence: true, uniqueness: true
+
+	#validates the body if is null 
+	#validates if the body has a size  minimum 20 letters
+	validates :body, presence: true, length: {minimum: 20}
 end
