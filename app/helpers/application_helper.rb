226eb4008ec7a0210311803db47706354dd94421
buirai <buirai_naetle@hotmail.com>
2015-07-25 15:41:06 -0500

    show banner of the articles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+	#url: thr url from the Image
+	#options: a hash of options
+	def article_cover url, options = {}
+		#class of the header
+		html_class  =options[:class]
+		html_style  = "background:url(#{url});"\
+		"width:100%;height:400px;background-size:cover;"
+
+		html = "<header class='#{html_class}' style='#{html_style}'>"\
+			"</header>"
+		html.html_safe
+	end
 end
