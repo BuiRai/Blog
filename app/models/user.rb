ebf838ea9656bacf997da38d185d533dd758f2a5
buirai <buirai_naetle@hotmail.com>
2015-07-25 15:08:23 -0500

    Show Avatar from gravatar in the comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,21 @@ class User < ActiveRecord::Base
  	has_many :articles #One to Many, a user can have a lot of articles
  	has_many :comments #One to Many, a user can have a lot of comments
  	include PermissionsConcern
+
+ 	#Avatar with Gravatar
+ 	#If you want show your avatar you need sign in 
+ 	#in Gravatar an change your avatar profile
+ 	#To more information visit: 
+ 	#https://es.gravatar.com/site/implement/images/ruby/
+
+ 	def avatar
+ 		# get the email from URL-parameters or what have you and make lowercase
+		email_address = self.email.downcase
+		 
+		# create the md5 hash
+		hash = Digest::MD5.hexdigest(email_address)
+		 
+		# compile URL which can be used in <img src="RIGHT_HERE"...
+		image_src = "http://www.gravatar.com/avatar/#{hash}"
+ 	end
 end
